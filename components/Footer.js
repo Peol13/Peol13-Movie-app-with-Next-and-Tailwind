123cb6032fd2a9abd9a80a608c76865718f9cde3
Mateusz <86568486+Peol13@users.noreply.github.com>
2022-06-22 15:59:44 +0200

    Update Footer.js

--- components/Footer.js
+++ components/Footer.js
@@ -3,7 +3,7 @@
 const Footer = () => {
   return (
     <div className="text-center h-32 flex items-center justify-center">
-      <p className="text-xl text-gray-800">&copy; Stwożone przez <a href="https://github.com/Peol13" target="bold" className="font-bold hover:text-blue-700">Peol13</a></p>
+      <p className="text-xl text-gray-800">&copy; Stworzone przez <a href="https://github.com/Peol13" target="bold" className="font-bold hover:text-blue-700">Peol13</a></p>
     </div>
   )
 }
